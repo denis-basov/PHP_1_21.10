928046d245594bd19399b09f73cc0997a8c53a88
Denis <52415012+denis-basov@users.noreply.github.com>
2024-10-21 22:15:47 +0300

    Add files via upload

---
+++ day_1/index2.php
@@ -0,0 +1,82 @@
+<?php
+
+// phpinfo();
+
+// $age = 22;
+// $userName = 'Иван';
+
+// $userName = 'Светлана';
+// $message = 'Привет, ' . $userName . '!';
+
+// $numbers = [3, 4, 7, 2, 4, 9]; // массив
+
+
+// echo "<pre>";
+// print_r($numbers);
+// echo "</pre>";
+
+// echo $numbers[5];
+
+// var_dump(3 === '3');
+
+/*
+$dogs = 3;
+$cats = 4;
+$animals = $cats + $dogs;
+
+var_dump($animals);*/
+
+/*
+$cats = 6;
+
+$cats = $cats + 2;
+var_dump($cats);*/
+
+/*
+$cats = 6;
+
+$cats += 2;
+var_dump($cats);*/
+
+/*
+$cats = 6;
+
+$cats = $cats + 1;
+$cats += 1;
+$cats++; //  уведичение на 1
+$cats--; // уменьшение на 1
+var_dump($cats);*/
+
+/*
+$cats = 6;
+var_dump($cats++);
+var_dump($cats); // 7
+*/
+
+/*
+$cats = 6;
+var_dump(++$cats);*/
+
+/**
+ * логические операторы
+ * И, ИЛИ, НЕ
+ * &&  ||   !
+ */
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2><?php //echo $message 
+        ?></h2>
+</body>
+
+</html>
